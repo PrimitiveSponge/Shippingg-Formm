1bf0ce5882f2dec478b74052620536e824c6cb05
dd17033 <dd17033@ms.wasatch.edu>
2022-11-28 09:57:34 -0700

    changes
    changed

---
+++ function.js
@@ -0,0 +1,30 @@
+function getToast (){
+
+    window.alert("wtf are you doing here? get out");
+
+}
+
+
+function putToast (){
+
+    window.alert("liar")
+
+}
+
+function theToast (){
+
+    window.alert("Damn your short")
+
+}
+
+function yesToast (){
+
+    window.alert("your a child molester aren't you?")
+
+}
+
+function notToast (){
+
+    window.alert("nice")
+
+}
